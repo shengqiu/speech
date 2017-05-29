72983e2fb00f1ce99f84fae48926e9183aefcb83
shengqiu <shengqiu.purdue@gmail.com>
2017-05-29 18:17:09 -0400

    add content

---
+++ speech2text.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+import os
+import speech_recognition as sr
+import time
+import datetime
+startTime = datetime.datetime.now()
+i = 1
+apiCredential = {
+  "url": "https://stream.watsonplatform.net/speech-to-text/api",
+  "username": "70b432fa-16a5-4e48-b8ef-8d62dc567214",
+  "password": "sii4STX8hd1W"
+}
+audioDir = 'audio'
+textDir = 'text'
+for name in os.listdir(audioDir):
+    print("%d %s transforming..." % (i, name))
+    r = sr.Recognizer()
+    try:
+        with sr.WavFile(audioDir + '/' + name) as source:
+            audio = r.record(source)
+            IBM_USERNAME = apiCredential['username']
+            IBM_PASSWORD = apiCredential['password']
+            text = r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD, language='en-US')
+            print(text)
+            open(textDir + '/' + name, 'a+').write(text)
+            time.sleep(5)
+            tempTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print('%s %d %s finished.' % (tempTime, i, name))
+
+    except Exception as e:
+        print(e)
+        tempTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print('%s %d %s failed.' % (tempTime, i, name))
+        continue
+endTime = datetime.datetime.now()
+last = endTime - startTime
+print('Time used： %s' % last)
+
+
